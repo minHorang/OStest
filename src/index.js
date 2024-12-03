5409dfae572975423dfafd7b2202d00c7e4b8e1c
M2 <m2@gimminbeoms-MacBook-Pro.local>
2024-12-03 12:24:11 +0900

    수정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import Router from './router'; // HashRouter를 사용한 라우터 정의
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Router />
   </React.StrictMode>
 );
